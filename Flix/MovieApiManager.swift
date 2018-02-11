9b3d53450f62110bc4d898a2a47f86bc55700318
Harjas Monga <harjasmonga@gmail.com>
2018-02-11 03:27:10 -0500

    added popular movies function to movies api

--- Flix/MovieApiManager.swift
+++ Flix/MovieApiManager.swift
@@ -35,6 +35,21 @@ class MovieApiManager {
         }
         task.resume()
     }
+    func popularMovies(completion: @escaping ([Movie]?, Error?) -> ()) {
+        let url = URL(string: MovieApiManager.baseUrl + "popular?api_key=\(MovieApiManager.apiKey)")!
+        let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
+        let task = session.dataTask(with: request) { (data, response, error) in
+            if let data = data {
+                let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
+                let movieDictionaries = dataDictionary["results"] as! [[String: Any]]
+                let movies = Movie.movies(dictionaries: movieDictionaries)
+                completion(movies, nil)
+            } else {
+                completion(nil, error)
+            }
+        }
+        task.resume()
+    }
     func superHeroMovies(completion: @escaping ([Movie]?, Error?) -> ()) {
         let url = URL(string: "https://api.themoviedb.org/3/discover/movie?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_genres=12%2C%2028%2C%20878&without_genres=14%2C%2053")!
         let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
